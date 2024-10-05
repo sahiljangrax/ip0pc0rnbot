8bb81033aebdc8442c895cd6321875ff07d70eaf
sahiljangrax <157376542+sahiljangrax@users.noreply.github.com>
2024-10-05 17:19:03 +0530

    hi

--- plugins/Extra/Redeem.py
+++ plugins/Extra/Redeem.py
@@ -44,7 +44,7 @@ Aᴍᴏᴜɴᴛ:</b> {num_codes}
 
         keyboard = InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="http://t.me/NehaTestBot")],
+                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="https://t.me/LonelyMoviesBots")],
                 [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/LMOWNERBOT")]
             ]
         )
